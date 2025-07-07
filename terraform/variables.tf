ec5ec1656d0182872350cf84b5c7f8697af29e41
leosch1 <35605161+leosch1@users.noreply.github.com>
2025-07-07 16:21:13 +0900

    comment out vars for now

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
-variable "domain_name" {
-  description = "The root domain name"
-  type        = string
-  default     = "viberadar.io"
-}
+# variable "domain_name" {
+#   description = "The root domain name"
+#   type        = string
+#   default     = "viberadar.io"
+# }
 
-variable "posthog_api_key" {
-  description = "API key for posthog"
-  type        = string
-  sensitive   = true
-}
+# variable "posthog_api_key" {
+#   description = "API key for posthog"
+#   type        = string
+#   sensitive   = true
+# }
