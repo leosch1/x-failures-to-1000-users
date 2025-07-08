3f9306c23ad92476fc87887be78acc557fe97525
leosch1 <35605161+leosch1@users.noreply.github.com>
2025-07-08 11:37:25 +0900

    formatting

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -10,23 +10,18 @@ resource "aws_cloudfront_distribution" "frontend" {
   enabled             = true
   is_ipv6_enabled     = true
   default_root_object = "index.html"
-
-  aliases = [var.domain_name]
-
+  aliases             = [var.domain_name]
   origin {
     domain_name = aws_s3_bucket.frontend.bucket_regional_domain_name
     origin_id   = "s3-frontend"
 
     origin_access_control_id = aws_cloudfront_origin_access_control.frontend_oac.id
   }
-
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "s3-frontend"
-
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "s3-frontend"
     viewer_protocol_policy = "redirect-to-https"
-
     forwarded_values {
       query_string = false
       cookies {
@@ -36,14 +31,12 @@ resource "aws_cloudfront_distribution" "frontend" {
   }
 
   price_class = "PriceClass_100"
-
   viewer_certificate {
     acm_certificate_arn            = aws_acm_certificate.frontend.arn
     ssl_support_method             = "sni-only"
     minimum_protocol_version       = "TLSv1.2_2021"
     cloudfront_default_certificate = false
   }
-
   restrictions {
     geo_restriction {
       restriction_type = "none"
