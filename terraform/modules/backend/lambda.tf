5a25137a43e1bc3dfbb06763adbee790dc1b0e7c
leosch1 <35605161+leosch1@users.noreply.github.com>
2025-07-17 12:34:52 +0900

    use newest lambda module version

--- terraform/modules/backend/lambda.tf
+++ terraform/modules/backend/lambda.tf
@@ -1,14 +1,15 @@
 ### Lambda Function for /userCount ###
 
 module "lambda_user_count" {
-  source  = "terraform-aws-modules/lambda/aws"
-  version = "~> 6.0"
-
-  function_name = "user-count"
-  handler       = "handler.lambda_handler"
-  runtime       = "python3.11"
-  source_path   = "../../../backend/lambdas/user-count"
-
+  source                        = "terraform-aws-modules/lambda/aws"
+  version                       = "~> 8.0"
+  function_name                 = "user-count"
+  handler                       = "handler.lambda_handler"
+  runtime                       = "python3.11"
+  source_path                   = "../../../backend/lambdas/user-count"
+  architectures                 = ["arm64"]
+  create_package                = true
+  package_type                  = "Zip"
   create_role                   = true
   attach_cloudwatch_logs_policy = true
 }
